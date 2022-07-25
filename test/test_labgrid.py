ef0c8f4310287a3e8804a60b3ca655fd25f621e0
khoitd1997 <khoidinhtrinh@gmail.com>
2022-07-25 10:40:43 -0700

    added power test

--- test/test_labgrid.py
+++ test/test_labgrid.py
@@ -1,7 +1,28 @@
 import labgrid.target
-from labgrid.driver import ShellDriver, SSHDriver
+from labgrid.driver import (
+    ShellDriver,
+    SSHDriver,
+    ManualPowerDriver,
+    ExternalPowerDriver,
+)
 
-# ExternalPowerDriver
+import time
+import pytest
+
+
+def test_external_power(target: labgrid.target.Target):
+    driver: ExternalPowerDriver = target.get_driver("ExternalPowerDriver")
+    driver.on()
+    time.sleep(5)
+    driver.off()
+
+
+@pytest.mark.skip(reason="Require manual so only test when needed")
+def test_manual_power(target: labgrid.target.Target):
+    driver: ManualPowerDriver = target.get_driver("ManualPowerDriver")
+    driver.on()
+    time.sleep(5)
+    driver.off()
 
 
 def test_serial_echo(target: labgrid.target.Target):
